4d79c1020c081e883cc471b364c5374c4c3f59e0
FL550 <max.fermor@gmail.com>
2021-02-06 13:39:07 +0100

    Adds argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 mod display;
 mod sensors;
 
-use std::env;
-
+use argh::FromArgs;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
+#[derive(FromArgs)]
+/// Reach new heights.
+struct CommandLineOptions {
+    /// whether or not to jump
+    #[argh(switch, short = 'd')]
+    debug: bool,
+}
+
 fn main() -> anyhow::Result<()> {
-    let mut debug = false;
-    let args_count = env::args().count();
+    let options: CommandLineOptions = argh::from_env();
 
     let running = Arc::new(AtomicBool::new(true));
     let r = running.clone();
@@ -21,9 +27,8 @@ fn main() -> anyhow::Result<()> {
     .expect("Error setting Ctrl-C handler");
 
     println!("Starting presence_detection");
-    if args_count > 1 {
+    if options.debug {
         println!("Logging on");
-        debug = true;
     }
     let mut ultrasonic_sensor = sensors::UltrasonicSensor::new()?;
     let mut display_manager = display::Manager::new();
@@ -32,8 +37,8 @@ fn main() -> anyhow::Result<()> {
     display_manager.turn_off_countdown()?;
 
     while running.load(Ordering::SeqCst) && run {
-        match check_distance(&mut ultrasonic_sensor, debug) {
-            Ok(true) => display_manager.turn_on(debug)?,
+        match check_distance(&mut ultrasonic_sensor, options.debug) {
+            Ok(true) => display_manager.turn_on(options.debug)?,
             Ok(false) => display_manager.turn_off_countdown()?,
             Err(e) => {
                 if let sensors::SensorError::TimeoutError = e {
@@ -65,6 +70,9 @@ fn check_distance(
             println!("Second distance is {}", distance);
         }
         if distance < 120 {
+            if debug {
+                println!("Display on");
+            }
             return Ok(true);
         }
     }
